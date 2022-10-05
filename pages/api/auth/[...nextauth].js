83f6e59a59245244f707e0e71a774b09281ec079
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2022-10-05 19:04:35 +0530

    SanityClient was not defined but now its fixed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { SanityAdapter } from "next-auth-sanity";
+import { sanityClient } from "../../../sanity";
 
 export const authOptions = {
   // Configure one or more authentication providers
